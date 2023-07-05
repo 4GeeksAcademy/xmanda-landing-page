2df651742a51761d52bff7d3ffc925b769f9ee31
Amanda Esquivel <128640315+xmanda@users.noreply.github.com>
2023-07-05 22:47:31 +0000

    final final

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark">
+    <nav className="navbar navbar-expand-lg bg-dark sticky-top">
   <div className="container-fluid mx-5 px-5">
     <a className="navbar-brand text-light" href="#">Tiny Art Gallery</a>
-    <div className="collapse navbar-collapse" id="navbarNav">
+    <div className="navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ms-auto">
         <li className="nav-item">
           <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
